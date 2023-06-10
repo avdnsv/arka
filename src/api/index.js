ab422da85572f90d12baa6ad5c36d82d16b6b228
avdnsv <avdnsv@gmail.com>
2023-06-10 18:07:12 +0300

    add api to backend request urls

--- src/api/index.js
+++ src/api/index.js
@@ -2,21 +2,21 @@ const base = import.meta.env.VITE_IP;
 
 export default {
   getListOfCities: () => {
-    return fetch(`${base}/cities`).then((res) => res.json());
+    return fetch(`${base}/api/cities`).then((res) => res.json());
   },
 
   getDataOfTheCity: (id) => {
-    return fetch(`${base}/city?id=` + id).then((res) => res.json());
+    return fetch(`${base}/api/city?id=` + id).then((res) => res.json());
   },
 
   getHumidMode: (temperature, humidity, humidityZone) => {
     return fetch(
-      `${base}/mode?temperature=${temperature}&humidity=${humidity}&humidity_zone=${humidityZone}`
+      `${base}/api/mode?temperature=${temperature}&humidity=${humidity}&humidity_zone=${humidityZone}`
     ).then((res) => res.text());
   },
 
   requestOfCalculation: (params) => {
-    return fetch(`${base}/calc`, {
+    return fetch(`${base}/api/calc`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=UTF-8",
@@ -26,7 +26,7 @@ export default {
   },
 
   requestOfReport: (params) => {
-    return fetch(`${base}/report`, {
+    return fetch(`${base}/api/report`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=UTF-8",
